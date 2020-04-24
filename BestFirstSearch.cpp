61855d202b4222e18718829e8195f7e8e7d907d0
jjacob44 <jjacob44@binghamton.edu>
2020-04-23 21:59:10 -0500

    infinite loop

--- BestFirstSearch.cpp
+++ BestFirstSearch.cpp
@@ -18,7 +18,7 @@ struct CompareBound{ //implementation of comparator overload
     return n1.bound < n2.bound;
   }
 };
-void calculate_bound(Node);
+int calculate_bound(Node);
 //priority queue implementation based on https://www.geeksforgeeks.org/stl-priority-queue-for-structure-or-class/
 priority_queue<Node,vector<Node>,CompareBound> my_queue;
 void sort_items();
@@ -29,7 +29,7 @@ struct item{
   double ratio; //profit/weight ratio
 };
 vector<item> items;
-item blank;
+/*item blank;
 blank.profit = 0;
 blank.weight = 0;
 blank.ratio = 0;
@@ -39,11 +39,11 @@ first.level = 0;
 first.profit = 0;
 first.weight = 0;
 first.bound = 0;
-first.items_used = items;
+first.items_used = items;*/
 void sort_items(){//bubble sort this array based on ratio
-  for(int i = 0; i<items.size();i++){
-    for(int j = 0;j<items.size();j++){
-      if (items[j].ratio< items[j+1].ratio){
+  for(int i = 1; i<items.size();i++){
+    for(int j = 1;j<items.size();j++){
+      if (items[j].ratio> items[j+1].ratio){
         item temp = items[j];
         items[j] = items[j+1];
         items[j+1] = temp;
@@ -51,9 +51,9 @@ void sort_items(){//bubble sort this array based on ratio
     }
   }
 }
-void calculate_bound(Node my_node){ //to be used for priority queue comparison
+int calculate_bound(Node my_node){ //to be used for priority queue comparison
   if (my_node.weight>=capacity){ //no need to do anything if we can't add anything else into the knapsack!
-    return;
+    return 0;
   }
   int weight_used = my_node.weight; //weight already in the bag
   long retVal = my_node.profit;
@@ -64,32 +64,47 @@ void calculate_bound(Node my_node){ //to be used for priority queue comparison
         weight_used+=items[i].weight;
       }
       else if(weight_used+items[i].weight> capacity){ //this would be the last item you could insert
-         long available = capacity-weight_used;
-         float fraction = available/items[i].weight;
-         retVal+= fraction * items[i].profit;
-         weight_used+= fraction * items[i].weight;
-         my_node.bound = retVal;
-         return;
+         int available = capacity-weight_used;
+         int fraction = available * (items[i].profit/items[i].weight) ;
+         retVal+= fraction;
+         //weight_used+= fraction * items[i].weight;
+         //my_node.bound = retVal;
+         return retVal;
       }
     }
   }
-  my_node.bound = retVal;
-  return;
+  //my_node.bound = retVal;
+  return retVal;
 }
 int main(int argc, char * argv[]){
-
-
+  int max_profit = 0;
+  Node best_node;
+  int leaves;
+  //constructing blank item for root node:
+  item blank;
+  blank.profit = 0;
+  blank.weight = 0;
+  blank.ratio = 0;
+  //items.push_back(blank);
+  //constructing root node:
+  Node root;
+  root.level = -1;
+  root.profit = 0;
+  root.weight = 0;
+  root.bound = 0;
+  root.items_used.push_back(0);
+  best_node = root;
   ifstream infile(argv[1]); //input file
   string first; //holds the number of items and the capacity
   infile >> first; //read in first line from input file
   size_t comma = first.find(","); //the comma delimiter. Will be recycled later...
   num_items = stoi(first.substr(0,comma));
   capacity =  stoi(first.substr(comma+1));
-
-  //for debug:
-  std::cout<<"# of items: "<<num_items<<", capacity: "<<capacity<<endl;
-  string temp; //contains the profit and weight of ith item
+  //std::cout<<"# of items: "<<num_items<<", capacity: "<<capacity<<endl;
+  string temp; //contains the profit and weight of ith item*/
   item my_item; //item to be added to vector of items
+
+  items.push_back(blank);
   for (int i = 0;i<num_items; i++){
     infile >> temp;
     comma = temp.find(","); //re-using from when it was first declared above
@@ -98,11 +113,41 @@ int main(int argc, char * argv[]){
     my_item.ratio = my_item.profit/my_item.weight;
     items.push_back(my_item);
   }
+
   sort_items();//performs the bubble sort
+  my_queue.push(root); //enqueue dummy node
+  int nodes_visited = 0;
+  //cout<<"got to this point"<<endl;
+  while (!my_queue.empty()){
+    Node current = my_queue.top();
+    my_queue.pop();
+    Node next;
+    next.level = current.level+1;
+    next.profit = current.profit+items[next.level].profit;
+    next.weight = current.weight + items[next.level].profit;
+    next.bound = calculate_bound(next);
+    //cout<<"bound: "<<next.bound<<endl;
+    if (next.weight <= capacity&& next.bound > max_profit){ //if the node is promising
+      best_node = next;
+      max_profit = best_node.profit;
+      my_queue.push(next);
+      continue;
+
+    }
+
+    next.items_used.push_back(next.level);
+    next.level = current.level;
+    next.profit = current.profit;
+    next.weight = current.weight;
+    next.bound = calculate_bound(next);
+    if (next.bound>next.profit){
+      my_queue.push(next);
+    }
+  }
 
 
   //for debug:
-/*  std::cout<<"-------ITEMS-------"<<endl;
+ std::cout<<"-------ITEMS-------"<<endl;
   for(int i=0;i<items.size();i++){
     cout<<"("<<items[i].weight<<","<<items[i].profit<<")"<<endl;
   }
@@ -110,5 +155,6 @@ int main(int argc, char * argv[]){
   std::cout<<"-------SORTED-------"<<endl;
   for(int i = 0; i<items.size();i++){
     cout<<"("<<items[i].weight<<","<<items[i].profit<<")"<<endl;
-  }*/
+  }
+  std::cout<<"max profit: "<<best_node.profit<<endl;
 }
